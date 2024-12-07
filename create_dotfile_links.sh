13d7de0c10681bbf8316bf4bda71f9be0cf32d5f
hntk03 <hntk03@gmail.com>
2024-12-07 17:35:10 +0900

    add vim plugins

--- create_dotfile_links.sh
+++ create_dotfile_links.sh
@@ -14,6 +14,10 @@ ln -sv "$CURRENT_DIR/alacritty.toml" "$CONFIG_ALACRITTY_DIR"
 
 # tmux
 ln -sv "$CURRENT_DIR/tmux.conf" "$HOME/.tmux.conf"
+ln -sv "$CURRENT_DIR/template" "$HOME/.vim"
+ln -sv "$CURRENT_DIR/plugins.toml" "$HOME/.vim/dein/userconfig"
+ln -sv "$CURRENT_DIR/pluginslazy.toml" "$HOME/.vim/dein/userconfig"
+
 
 # vim
 ln -sv "$CURRENT_DIR/vimrc" "$HOME/.vimrc"
---
+++ template/cpp/base-mymain.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+using ll = long long;
+
+//container util
+#define SORT(c) sort((c).begin(),(c).end())
+#define all(a) (a).begin(), (a).end()
+
+//repetition
+#define FOR(i,a,b) for(int i=(a);i<(b);++i)
+#define REP(i,n)  FOR(i,0,n)
+
+//print
+#define pv(val) cerr << #val << '=' << (val) << endl
+
+const int INF = 1e9;
+
+int main(void){
+
+	{{_cursor_}}
+
+
+	return 0;
+
+}
