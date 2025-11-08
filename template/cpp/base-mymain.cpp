4b94948baa197c219b11220e4a51f19bf8b7e8fd
hntk03 <hntk03@gmail.com>
2025-11-08 21:53:58 +0900

    apply clang-format

--- template/cpp/base-mymain.cpp
+++ template/cpp/base-mymain.cpp
@@ -3,24 +3,23 @@
 using namespace std;
 using ll = long long;
 
-//container util
-#define SORT(c) sort((c).begin(),(c).end())
+// container util
+#define SORT(c) sort((c).begin(), (c).end())
 #define all(a) (a).begin(), (a).end()
 
-//repetition
-#define FOR(i,a,b) for(int i=(a);i<(b);++i)
-#define REP(i,n)  FOR(i,0,n)
+// repetition
+#define FOR(i, a, b) for (int i = (a); i < (b); ++i)
+#define REP(i, n) FOR(i, 0, n)
 
-//print
+// print
 #define pv(val) cerr << #val << '=' << (val) << endl
 
 const int INF = 1e9;
 
-int main(void){
+int main(void) {
 
-	{{_cursor_}}
+  {{_cursor_}}
 
-
-	return 0;
-
+  return 0;
 }
+
