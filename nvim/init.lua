4343e4a550a109968c0f45f7542bc8cc7a31947c
hntk03 <hntk03@gmail.com>
2026-02-05 21:47:33 +0900

    feat(nvim): ALEの設定を追加

--- nvim/init.lua
+++ nvim/init.lua
@@ -41,6 +41,37 @@ require('jetpack.paq') {
   {'neovim/nvim-lspconfig'}, -- lsp
 }
 
+-- ALE
+vim.g.ale_virtualtext_cursor = 'disabled'
+vim.g.ale_sign_column_always = 1
+vim.g.ale_completion_enabled = 1
+vim.g.ale_sign_error = '⨉'
+vim.g.ale_sign_warning = '⚠'
+vim.g.ale_echo_msg_format = '[%linter%] %s [%severity%]'
+vim.g.ale_statusline_format = {'⨉ %d', '⚠ %d', '⬥ ok'}
+vim.g.ale_cpp_cc_options = "-Wall -std=c++20"
+vim.g.ale_linters = {
+   c = {'gcc', 'cppcheck', 'clangtidy'},
+   cpp = {'gcc', 'cppcheck', 'clangtidy'},
+   markdown = {'markdownlint', 'textlint'},
+   python = {'ruff', 'mypy'},
+}
+vim.g.ale_fixers = { 
+ c = {'clang-format'},
+ cpp = {'clang-format'},
+ markdown = {'prettier'},
+ javascript = {'prettier'},
+ astro = {'prettier'},
+ css = {'prettier'},
+ sh = {'shfmt'},
+}
+vim.g.ale_fix_on_save = 1
+
+-- vim-astro
+vim.g.astro_typescript = 'enable'
+vim.g.astro_stylus = 'enable'
+
 vim.lsp.enable('clangd')
 
 vim.cmd("colorscheme tender")
+vim.g.lightline = { colorscheme = 'tender' }
