1d75fe2b64c3fe034953ea96b612919a4ee66804
Ohno.T <46061787+hntk03@users.noreply.github.com>
2026-08-16 14:59:43 +0900

    chore: cppのテンプレートを変更

--- nvim/templates/cpp/base-mymain.cpp
+++ nvim/templates/cpp/base-mymain.cpp
@@ -3,18 +3,9 @@
 using namespace std;
 using ll = long long;
 
-// container util
-#define SORT(c) sort((c).begin(), (c).end())
-#define all(a) (a).begin(), (a).end()
-
 // repetition
 #define FOR(i, a, b) for (int i = (a); i < (b); ++i)
 #define REP(i, n) FOR(i, 0, n)
-
-// print
-#define pv(val) cerr << #val << '=' << (val) << endl
-
-const int INF = 1e9;
 
 int main(void) {
 
@@ -22,4 +13,3 @@ int main(void) {
 
   return 0;
 }
-
