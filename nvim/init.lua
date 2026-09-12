3fea996da59e22fafc79a493dcb00906fe92ed04
Ohno.T <46061787+hntk03@users.noreply.github.com>
2026-09-12 16:57:11 +0900

    feat(nvim): luaのフォーマッターを追加

--- nvim/init.lua
+++ nvim/init.lua
@@ -67,6 +67,7 @@ vim.g.ale_fixers = {
  sh = {'shfmt'},
  python = {'ruff_format'},
  go = {'gofmt'},
+  lua = { "stylua" },
 }
 vim.g.ale_markdown_markdownlint_executable = 'markdownlint-cli2'
 vim.g.ale_fix_on_save = 1
