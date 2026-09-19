ca066fa817bc6dfc744c848b1d8f47adb018dfa1
Ohno.T <46061787+hntk03@users.noreply.github.com>
2026-09-19 16:58:39 +0900

    refactor: colorsの変数を使用するように変更

--- nvim/lua/colorscheme.lua
+++ nvim/lua/colorscheme.lua
@@ -1,4 +1,5 @@
 vim.cmd("colorscheme tender")
 vim.g.lightline = { colorscheme = "tender" }
 
-vim.api.nvim_set_hl(0, "Operator", { fg = "#eeeeee" })
+local colors = require("colors")
+vim.api.nvim_set_hl(0, "Operator", { fg = colors.text })
